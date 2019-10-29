c4250a458e14692be37a9db8d980c7743242e657
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-10-29 16:50:51 +0000

    Remove addons

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -344,7 +344,7 @@ then
 	ADDONS_GAME="game.libretro.mame2000 game.libretro.mame2003 game.libretro.scummvm game.libretro game.libretro.2048 game.libretro.beetle-gba game.libretro.fceumm game.libretro.gw game.libretro.beetle-pce-fast game.libretro.bnes game.libretro.nestopia game.libretro.bsnes-mercury-balanced game.libretro.bluemsx game.libretro.vecx game.libretro.gambatte game.libretro.beetle-bsnes game.libretro.meteor game.libretro.mgba game.libretro.vba-next game.libretro.quicknes game.libretro.snes9x game.libretro.stella game.libretro.dosbox game.libretro.vbam game.libretro.genplus"
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]
 	then
-	    ADDONS_TO_BUILD="${ADDONS_AUDIO_ENCODERS} ${ADDONS_INPUTSTREAM}"
+	    ADDONS_TO_BUILD="${ADDONS_AUDIO_ENCODERS}"
 	    PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vc/include:/opt/vc/include/interface:/opt/vc/include/interface/vcos/pthreads:/opt/vc/include/interface/vmcs_host/linux -DCMAKE_LIBRARY_PATH=/opt/vc/lib"
 	fi
 	if [ "$1" == "vero2" ]
