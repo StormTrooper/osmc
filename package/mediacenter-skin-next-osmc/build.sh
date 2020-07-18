cc26deb3749705cdda8e7a0f1cf2536642f2de9b
Greg McCarthy <greg@gjmccarthy.co.uk>
2020-07-18 21:12:59 +0100

    Update build script for skin.osmc.scope

--- package/mediacenter-skin-next-osmc/build.sh
+++ package/mediacenter-skin-next-osmc/build.sh
@@ -7,24 +7,29 @@
 
 make clean
 
-REV="c3e77883aeedef769b5187d85a4ac70534e58ae4"
-
-echo -e "Building package mediacenter-skin-osmc"
-echo -e "Downloading skin"
-pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
-if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
-pushd src/skin.osmc-*
-install_patch "../../patches" "all"
-popd
-echo -e "Moving files in to place"
+process_skin() {
+echo -e "Moving ${2} files in to place"
 mkdir -p files/usr/share/kodi/addons
-cp -ar src/skin.osmc-${REV}/ files/usr/share/kodi/addons/skin.osmc
-if [ -f files/usr/share/kodi/addons/skin.osmc/media/Textures.xbt ]
+cp -ar src/skin.osmc-${1} files/usr/share/kodi/addons/${2} # Always called skin.osmc in src because of repo name
+if [ -f files/usr/share/kodi/addons/${2}/media/Textures.xbt ]
 then
     echo "TexturePacked file detected, deleting unneeded artefacts"
-    pushd files/usr/share/kodi/addons/skin.osmc/media
+    pushd files/usr/share/kodi/addons/${2}/media
     find . ! -name 'Textures.xbt' -delete
     popd
 fi
+}
 
+
+REV="c3e77883aeedef769b5187d85a4ac70534e58ae4"
+SCOPE_REV="76b8be7cea956c6dc0e124df31fbb8d8cd28b4e7"
+echo -e "Building package mediacenter-skin-osmc"
+echo -e "Downloading skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading classic skin" && exit 1; fi
+process_skin "$REV" "skin.osmc"
+echo -e "Downloading scope skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${SCOPE_REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading classic skin" && exit 1; fi
+process_skin "$SCOPE_REV" "skin.osmc.scope"
 dpkg_build files/ mediacenter-skin-osmc.deb
