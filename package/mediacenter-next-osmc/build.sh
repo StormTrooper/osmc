57a066b86d7b9260d008d7a8112a029a3758d7cb
Greg McCarthy <greg@gjmccarthy.co.uk>
2021-01-04 17:26:53 +0000

    Update xbmc commit

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp2" ] || [ "$1" == "rbp4" ] || [ "$1" == "pc" ] || [ "$1" == "vero3" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/983a0193584441f1135e3544df3729b13af63e38.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/b783517b55d487bb392d25baf36e816a4c53993d.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 fi
 
